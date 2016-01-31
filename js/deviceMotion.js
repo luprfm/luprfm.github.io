4b1031934d6f98f725a6c2f334701ecf211bb4a0
luprfm <f2tayokosuka>
2016-01-31 14:58:55 +0900

    devicemotion

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -12,7 +12,7 @@ $(function(){
         $('#device li').eq(1).text('加速度Y: ' + y);
         $('#device li').eq(2).text('加速度Z: ' + z);
 
-        if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z)){
+        if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
             $('#result').text('振ったよ');
         }
 
