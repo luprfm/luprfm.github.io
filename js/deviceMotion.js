d098ebc56bd2cf7e26e0e6c8814e74e429b2d2d4
luprfm <f2tayokosuka>
2016-01-31 14:37:13 +0900

    devicemotion

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -9,8 +9,8 @@ $(function(){
         var z = evt.acceleration.z;
 
         $('#device li').eq(0).text('加速度X: ' + x);
-        $('#device li').eq(1).text('加速度X: ' + y);
-        $('#device li').eq(2).text('加速度X: ' + z);
+        $('#device li').eq(1).text('加速度y: ' + y);
+        $('#device li').eq(2).text('加速度z: ' + z);
 
     }
 
