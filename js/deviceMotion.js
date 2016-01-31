9e6db297a02f62e11959209f120a7f912b55a5bd
luprfm <f2tayokosuka>
2016-01-31 15:06:07 +0900

    devicemotion

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -1,23 +1,45 @@
 $(function(){
 
-// 加速度のイベント
-    window.addEventListener('devicemotion',shakeDevice);
+// // 加速度のイベント
+//     window.addEventListener('devicemotion',shakeDevice);
+//
+//     function shakeDevice(evt){
+//         var x = evt.acceleration.x;
+//         var y = evt.acceleration.y;
+//         var z = evt.acceleration.z;
+//
+//         $('#device li').eq(0).text('加速度X: ' + x);
+//         $('#device li').eq(1).text('加速度Y: ' + y);
+//         $('#device li').eq(2).text('加速度Z: ' + z);
+//
+//         if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
+//             $('#result').text('振ったよ');
+//         }
+//
+//
+//     }
 
-    function shakeDevice(evt){
-        var x = evt.acceleration.x;
-        var y = evt.acceleration.y;
-        var z = evt.acceleration.z;
-
-        $('#device li').eq(0).text('加速度X: ' + x);
-        $('#device li').eq(1).text('加速度Y: ' + y);
-        $('#device li').eq(2).text('加速度Z: ' + z);
-
-        if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
-            $('#result').text('振ったよ');
-        }
 
 
-    }
+
+// 傾き ジャイロセンサー
+window.addEventListener('deviceorientation',roteDevice);
+
+function roteDevice(evt){
+    // X軸
+    var x = evt.beta;
+    // Y軸
+    var y = evt.gamma;
+    // Z軸
+    var z = evt.alpha;
+
+    $('#device li').eq(0).text('傾きX: ' + x);
+    $('#device li').eq(1).text('傾きY: ' + y);
+    $('#device li').eq(2).text('傾きZ: ' + z);
+
+
+
+}
 
 
 });
