da5df78a2de8654607f6eb46b1ef34799fe6f0c7
luprfm <f2tayokosuka>
2016-01-31 16:25:22 +0900

    ajax

--- js/ajax.js
+++ js/ajax.js
@@ -16,12 +16,22 @@ $(function(){
             type:'GET',
             dataType:'html'
         }).done(function(data){
+            // 成功
             console.log(data);
+
+            showContent(data);
+
+
         }).fail(function(){
+            // 失敗
             console.log('ajax error');
         });
 
 
+        // 表示させる関数
+        function showContent(data){
+            $('#container').append(data);
+        }
 
 
     });
