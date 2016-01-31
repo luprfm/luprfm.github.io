337b61be4a5c48f963f931c18af94aaefc9f23ef
luprfm <f2tayokosuka>
2016-01-31 15:27:03 +0900

    touch

--- js/touch.js
+++ js/touch.js
@@ -2,17 +2,19 @@
 
 $(function(){
 
+
+
     // タッチイベント
     $('#ball').on('touchstart',function(){
-        $('touchResult li').eq(0).text('touchstart したよ')
+        $('#touchResult li').eq(0).text('touchstart したよ')
     });
 
     $('#ball').on('touchmove',function(){
-        $('touchResult li').eq(1).text('touchmove したよ')
+        $('#touchResult li').eq(1).text('touchmove したよ')
     });
 
     $('#ball').on('touchend',function(){
-        $('touchResult li').eq(2).text('touchend したよ')
+        $('#touchResult li').eq(2).text('touchend したよ')
     });
 
 
