43c4b734897232af13e482340918013082573872
luprfm <f2tayokosuka>
2016-01-31 15:37:09 +0900

    gps

--- js/touch.js
+++ js/touch.js
@@ -2,6 +2,27 @@
 
 $(function(){
 
+    // 緯度 経度
+    var lon;
+    var lat;
+
+
+    // GPS
+    navigator.geolocation.getCurrentPosition(
+        function(evt){
+            // 緯度
+            var lon = evt.coords.longitude;
+            // 経度
+            var lat = evt.coords.latitude;
+        },
+        function(error){
+        console.log('GPSとれなかったよー');
+    });
+
+
+
+
+
 
 
     // タッチイベント
@@ -16,6 +37,12 @@ $(function(){
     $('#ball').on('touchend',function(){
         $('#touchResult li').eq(2).text('touchend したよ')
     });
+
+
+
+    $('#geo li').eq(0).text('緯度: ' + lon);
+    $('#geo li').eq(1).text('経度: ' + lat);
+
 
 
 });
