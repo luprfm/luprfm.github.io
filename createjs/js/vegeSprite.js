d1b86ac0ec7ed9d3d98a94d046ad1eb548f91ce1
luprfm <f2tayokosuka>
2016-02-07 16:09:55 +0900

    []

--- createjs/js/vegeSprite.js
+++ createjs/js/vegeSprite.js
@@ -1,8 +1,8 @@
 var vitaminCsprite = {
-    images:'img/vitaminC.png',
+    images:['img/vitaminC.png'],
     frames:{width:160,height:160},
     animations:{
         stand:34,
         walk:[0,33]
-    };
-};
+    }
+}
