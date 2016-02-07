4843e85e2f35bbf886bfd4b2b93ffaf9cd4755f1
luprfm <f2tayokosuka>
2016-02-07 11:47:44 +0900

    weatheapi

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -18,11 +18,21 @@ $(function(){
             url:url,
             dataType:'json'
         }).done(function(data){
-            console.log(data);
+
+            showWeather(data);
 
         }).fail(function(){
             console.log('ajax error!!');
         });
+
+        // htmlに表示する関数
+        function showWeather(data){
+            console.log(data);
+
+            // 地域名
+            var areaName = data.name;
+            $('#areaName').text(areaName);
+        }
 
     });
 });
