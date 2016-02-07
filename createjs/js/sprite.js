2f5756ac1b900fedf43ef0fa25c16acd15cc47b2
luprfm <f2tayokosuka>
2016-02-07 16:07:32 +0900

    vitaminC

---
+++ createjs/js/sprite.js
@@ -0,0 +1,67 @@
+//先読みするリスト 画像や音などの読み込み
+var manifest = [
+    {src:'img/calcium.png'},
+    {src:'img/iron.png'},
+    {src:'img/vitaminC.png'}
+];
+
+
+
+
+// DOMを読み込んだら
+
+$(function(){
+
+    //ステージ（canvas)の設定をする
+        var stage = new createjs.Stage('canvasEl');
+
+
+        //loadQueue（ロードキュー）ローディングの設定
+        var loadQueue = new createjs.LoadQueue();
+        loadQueue.loadManifest(manifest); //読み込み開始
+
+
+        loadQueue.addEventListener('complete',function(){
+            console.log('読み込み完了!!!(^^)');
+            showVege();
+        });
+
+
+
+
+        function showVege(){
+            //スプライトシートの設定
+            var vcSprite = new createjs.SpriteSheet(vitaminCsprite);
+            //スプライトの設定
+            var vitaminC = new createjs.Sprite(vcSprite,'stand');
+            stage.addChild(vitaminC);
+            vitaminC.x = 150;
+            vitaminC.y = 480;
+            vitaminC.alpha = 0;
+
+            //アニメーション
+            createjs.Tween.get(vitaminC).to({alpha:1},500).wait(1000).to({x:500},4000);
+
+
+            stage.update();
+
+        };
+
+
+
+        //フレームレート(だいたい30〜60くらいが目安)
+        createjs.Ticker.setFPS(30);
+        createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+
+        //アニメーションの設定（Ticker 繰り返し回す）
+        createjs.Ticker.addEventListener('tick',function(){
+            //ステージの更新
+            stage.update();
+
+        });
+
+
+
+
+
+});
