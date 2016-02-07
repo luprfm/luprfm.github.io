0a7bc75a557027f6df9e5a16de063d2320812048
luprfm <f2tayokosuka>
2016-02-07 15:25:35 +0900

    250

--- createjs/js/bitmap.js
+++ createjs/js/bitmap.js
@@ -35,7 +35,7 @@ $(function(){
             stage.addChild(tomato);
 
             var pumpkin = new createjs.Bitmap(manifest[1].src);
-            pumpkin.x = 150;
+            pumpkin.x = 250;
             pumpkin.y = 100;
             stage.addChild(pumpkin);
 
