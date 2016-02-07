e663a6e2f22d57f6e1c73f80523127c86c4e9f6b
luprfm <f2tayokosuka>
2016-02-07 11:41:51 +0900

    weatherapi

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -3,6 +3,26 @@ $(function(){
     navigator.geolocation.getCurrentPosition(function(pos){
         var lat = pos.coords.latitude;
         var lon = pos.coords.longitude;
+
         console.log(lat,lon);
+
+        // 定数(値の上書きができなくなる)
+        const apikey = 'dd09803f2b0f1397cf528361aae11377';
+
+        // openweatermapのお天気情報のURL
+        var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apikey;
+
+
+
+        $.ajax({
+            url:url,
+            dataType:'json'
+        }).done(function(data){
+            console.log(data);
+
+        }).fail(function(){
+            console.log('ajax error!!');
+        });
+
     });
 });
